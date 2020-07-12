8900130e980926438365457bd05263b30bc14d51
Naryan Black <60332733+NaryanBlack98@users.noreply.github.com>
2020-07-11 20:45:09 -0700

    Endscene and Main Menu
    ^

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("MainGame");
+    }
+  
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+}
